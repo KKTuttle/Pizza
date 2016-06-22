82dd52f05a6ce61417344c3ee6ec90983c77cc1f
Katarina Tuttle <katka.karolcikova@gmail.com>
2016-06-22 14:56:57 -0700

    Delete comment-out code and edit spacing

--- js/scripts.js
+++ js/scripts.js
@@ -6,28 +6,9 @@ function Pizza (userName, size, toppings) {
   this.pizzaCost = 10;
   this.total =0;
 }
-// function Toppings(toppings, total) {
-//   this.toppings = toppings;
-//   // this.price = price;
-//   this.total =0;
-// }
 Pizza.prototype.allInfo=function() {
   return "Thank you for your order, " + this.userName +". Your order is a" + this.size +" pizza with " + this.toppings + ". Total cost: $" + this.pizzaCost + ".";
 };
-
-// Toppings.prototype.totalCost = function() {
-//   return "hey" + this.total +
-// }
-
-// var cennik = function (newCena) {
-//   if(newCena.toppings === "ham" || newCena.toppings === "bacon" || newCena.toppings === "pepperoni") {
-//     var total = newCena.total + 2;
-//   } else if (newCena.toppings === "pineapple" || newCena.toppings === "cheese" || newCena.toppings === "tomatoe" || newCena.toppings === "garlic") {
-//     var total = newCena.total + 0.5;
-//     return newCena.total;
-//   }
-// }
-
 var costCalculator = function (newOrder) {
   if (newOrder.size === "large") {
     newOrder.pizzaCost *= 2.5;
@@ -45,28 +26,12 @@ $(document).ready(function() {
     toppings =$("input:checkbox[name=toppings]:checked").map(function(_, toppings) {
       return $(toppings).val();
     }).get();
-    // var toppings =$("input:checkbox[name=toppings]:checked").map(function() {
-    //   return $(this).val();
-    // }).get();
 
-    // working only for one
-    // if ($(".toppings").is(":checked")) {
-    //   debugger;
-    //   toppings.push($(".toppings").val());
-    // }  else if($(this).is(":not(:checked)")) {
-    //     alert("Almost there, Katie");
-    // }
     var newOrder = new Pizza(userName, size, toppings);
     var finalInfo = costCalculator(newOrder);
-    // var newCena = new Toppings(toppings, total)
-    // var finalCost = cennik(newCena);
     var total =0;
-      // console.log(toppings);
-
     $("#userOrder").html("<li>" + newOrder.allInfo() + "</li>");
     $("#formPizza")[0].reset();
-    // $("#userOrder").remove();
-
     event.preventDefault();
   });
 });
