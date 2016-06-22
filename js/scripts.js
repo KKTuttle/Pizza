ea86c5c0e2d9d2f5a9622e96970a9e75c770630d
Katarina Tuttle <katka.karolcikova@gmail.com>
2016-06-22 15:45:06 -0700

    Separate business logic and UI

--- js/scripts.js
+++ js/scripts.js
@@ -4,34 +4,32 @@ function Pizza (userName, size, toppings) {
   this.size = size;
   this.toppings = toppings;
   this.pizzaCost = 10;
-  this.total =0;
+  this.total = 0;
 }
-Pizza.prototype.allInfo=function() {
-  return "Thank you for your order, " + this.userName +". Your order is a" + this.size +" pizza with " + this.toppings + ". Total cost: $" + this.pizzaCost + ".";
-};
-var costCalculator = function (newOrder) {
-  if (newOrder.size === "large") {
-    newOrder.pizzaCost *= 2.5;
-  } else if (newOrder.size === "medium") {
-    newOrder.pizzaCost *= 1.5;
-  } else if (newOrder.size === "small") {
-    newOrder.pizzaCost *= 1.3;
+Pizza.prototype.costCalculator = function () {
+  debugger;
+  if (this.size === "large") {
+    this.pizzaCost *= 2.5;
+  } else if (this.size === "medium") {
+    this.pizzaCost *= 1.5;
+  } else if (this.size === "small") {
+    this.pizzaCost *= 1.3;
   }
 };
 var toppings=[];
 $(document).ready(function() {
   $("form#formPizza").submit(function(event) {
-    var userName=$("#userName").val();
-    var size =$("input:radio[name=size]:checked").val();
-    toppings =$("input:checkbox[name=toppings]:checked").map(function(_, toppings) {
+    var userName= $("#userName").val();
+    var size = $("input:radio[name=size]:checked").val();
+    toppings = $("input:checkbox[name=toppings]:checked").map(function(_, toppings) {
       return $(toppings).val();
     }).get();
 
     var newOrder = new Pizza(userName, size, toppings);
-    var finalInfo = costCalculator(newOrder);
-    var total =0;
-    $("#userOrder").html("<li>" + newOrder.allInfo() + "</li>");
-    $("#formPizza")[0].reset();
+    var finalInfo = newOrder.costCalculator();
+    $("#userOrder").html("<li>" + "Thank you for your order, " + newOrder.userName +". Your order is a " + newOrder.size +" pizza with " + newOrder.toppings + ". Total cost: $" + newOrder.pizzaCost + "."  + "</li>");
+    var total = 0;
     event.preventDefault();
+    $("#formPizza")[0].reset();
   });
 });
